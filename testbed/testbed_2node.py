95096e0e48adef48a275aad394707204dfb4eefd
Mario <mario.engelbrecht@netronome.com>
2017-10-02 16:18:36 +0200

    update

--- testbed/testbed_2node.py
+++ testbed/testbed_2node.py
@@ -1,8 +1,8 @@
 from fabric.api import env
 
 #Management addresses of hosts in the cluster
-h = 'root@172.26.1.104'          # control & compute node - nfp
-d = 'root@172.26.1.105' # compute node - nfp
+HOST0 = 'root@172.26.1.126'  # control & compute node - nfp
+HOST1 = 'root@172.26.1.127' # compute node - nfp
 
 #External routers if any
 #for eg.
@@ -13,48 +13,48 @@ ext_routers = []
 router_asn = 64525
 
 #Host from which the fab commands are triggered to install and provision
-host_build = h
+host_build = HOST0
 
 #Role definition of the hosts.
 env.roledefs = {
-    'all': [h, d],
-    'cfgm': [h],
-    'openstack': [h],
-    'control': [h],
-    'compute': [d, h],
-    'collector': [h],
-    'webui': [h],
-    'database': [h],
+    'all': [HOST0, HOST1],
+    'cfgm': [HOST0],
+    'openstack': [HOST0],
+    'control': [HOST0],
+    'compute': [HOST1, HOST0],
+    'collector': [HOST0],
+    'webui': [HOST0],
+    'database': [HOST0],
     'build': [host_build],
-    'storage-master': [h],
-    'storage-compute': [d, h],
+    'storage-master': [HOST0],
+    'storage-compute': [HOST1, HOST0],
 }
 
 # required if using env.ns_agilio_vrouter
 control_data = {
-h : { 'ip': '172.18.48.32/24',
+HOST0 : { 'ip': '172.18.48.32/24',
 'gw' : '172.18.48.1',
 'device' :'nfp_p0'},
-d : { 'ip': '172.18.48.33/24',
+HOST1 : { 'ip': '172.18.48.33/24',
 'gw' : '172.18.48.1',
 'device' :'nfp_p0'},
 }
 
 # Setup Netronome Agilio vRouter on specified nodes
 env.ns_agilio_vrouter = {
-h: {'huge_page_alloc': '24G',
+HOST0: {'huge_page_alloc': '12G',
 'huge_page_size' : '1G',
 'coremask' : '2,4',
 'pinning_mode' : 'auto:split'},
-d: {'huge_page_alloc': '24G',
+HOST1: {'huge_page_alloc': '12G',
 'huge_page_size' : '1G',
 'coremask' : '2,4',
 'pinning_mode' : 'auto:split'}
 }
 
 env.hostnames = {
-    h: 'h',
-    d: 'd',
+    HOST0: 'HOST0',
+    HOST1: 'HOST1',
 }
 
 #Openstack admin password
@@ -62,15 +62,15 @@ env.openstack_admin_password = 'netronome'
 
 # Passwords of each host
 env.passwords = {
-    h: 'netronome',
-    d: 'netronome',
+    HOST0: 'netronome',
+    HOST1: 'netronome',
     host_build: 'netronome',
 }
 
 #For reimage purpose
 env.ostypes = {
-    h: 'ubuntu',
-    d: 'ubuntu',
+    HOST0: 'ubuntu',
+    HOST1: 'ubuntu',
 }
 
 env.enable_lbaas = True
