c5f11a9039495fd67c2b074eef7fc426310460ab
netronome-support <netronome-support@netronome.com>
2018-05-21 15:04:05 +0000

    Commit 009 Nick.vonklemperer@netronome.com Fixed kernel change script to exit process if kernel version is correct

--- scripts/0_kernel_downgrade.sh
+++ scripts/0_kernel_downgrade.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 # Script to downgrade kernel to Contrail 4.1.1 supported 4.4.0-62
+# Check if kernel needs to change
+echo "Checking kernel version"
+kern=$(uname -r)
+if [[ "$kern" == "4.4.0-62-generic" ]]; then
+	echo "Kernel is $kern no need to change"
+	exit -1
+else
+	echo "Changing kernel to 4.4.0-62"
+fi
+read -p "wait"
 # Download and install kernel files
 echo "Downloading files"
 mkdir kernelfiles
@@ -14,11 +24,8 @@ echo "Updating grub"
 sed -i '/GRUB_DEFAULT=/c\GRUB_DEFAULT="Advanced options for Ubuntu>Ubuntu, with Linux\4.4.0-62-generic"' /etc/default/grub
 sed -i '/GRUB_CMDLINE_LINUX=/c\GRUB_CMDLINE_LINUX="${GRUB_CMDLINE_LINUX_DEFAULT} \intel_iommu=on iommu=pt intremap=on default_hugepagesz=2M \hugepagesz=2M hugepages=8196"' /etc/default/grub
 update-grub
-
 # Cleaning up kernel
 echo "Removing existing kernel and cleaning up"
-# Get existing kernel version
-kern=$(uname -r)
 # Remove old kernel version
 apt-get -y purge linux-headers-$kern linux-image-$kern
 # Delete downloaded files
