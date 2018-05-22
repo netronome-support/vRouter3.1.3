6a5b0dc226a3c77dbb990821ad2384bbd2f69eeb
netronome-support <netronome-support@netronome.com>
2018-05-22 08:15:21 +0000

    Commit 011 Nick.vonklemperer@netronome.com Generalized install NFP script to take build version arguments...

--- scripts/1_install_vrouter_agilio.sh
+++ scripts/1_install_vrouter_agilio.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 # Script to install Agilio NFP drivers for vRouter4.1.1.
 # Install dependencies
-echo "Installing dependencies"
-apt-get -y install dkms python libjansson4 ethtool
-apt autoremove
+BUILD_VER=53
+CONTRAIL_VER=4.1
+BUILD_NAME="Netronome_R${CONTRAIL_VER}_build_${BUILD_VER}"
+
 # Download driver files
 echo "Downloading NFP drivers"
 mkdir NFP
-wget http://pahome.netronome.com/releases-intern/vrouter/builds/Netronome_R4.1_build_31.tar -O NFP/Netronome_R4.1_build_31.tar
+wget http://pahome.netronome.com/releases-intern/vrouter/builds/${BUILD_NAME}.tar -O NFP/${BUILD_NAME}.tar
 
 # Extract and install driver package
 echo "Installing driver"
-tar xvf NFP/Netronome_R4.1_build_31.tar -C NFP/
-cd NFP/Netronome_R4.1_build_*/repo_setup/debs/
+tar xvf NFP/${BUILD_NAME}.tar -C NFP/
+# Install dependencies
+echo "Installing depends"
+cd NFP/${BUILD_NAME}/repo_setup/debs/dependencies
+dpkg -i *.deb
+apt-get install -y -f
+apt-get autoremove -y
+# Installing NFP packages
+echo "Installing NFP packages"
+cd ..
 dpkg -i agilio-nfp-driver-dkms_2018.04.13.0400.237fdbb_all.deb\
 	 ns-agilio-vrouter-udev_4.1.0.0-*.deb agilio-nic-firmware-2.0.7-1.deb\
 	 nfp-bsp-6000-b0_2018.04.13.1600-1_amd64.deb 
@@ -34,6 +43,8 @@ update-initramfs -u
 
 # Cleaning up
 echo "Cleaning lose ends"
+cd ..
+cd ..
 cd ..
 cd ..
 rm -r NFP
