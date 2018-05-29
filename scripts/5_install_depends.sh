3ea1ad5303220ae67c8a61338d6fad90249e8eb8
netronome-support <netronome-support@netronome.com>
2018-05-29 12:13:57 +0000

    Commit 019 Nick.vonklemperer@netronome.com Changed depends

--- scripts/5_install_depends.sh
+++ scripts/5_install_depends.sh
@@ -2,7 +2,11 @@
 # Installs dependencies for server-manager
 echo "Installing server-manager dependenceies"
 apt-get install -y Linux-image-extra-$(uname -r)
-apt-get install -y python-nova=2:13.1.4-0ubuntu4.2 libnl-3-200=3.2.27-1 python python-minimal=2.7.11-1 libpython-stdlib=2.7.11-1 --allow-downgrades
-apt-get install -y python=2.7.11-1 python-dev=2.7.11-1 libpython-dev=2.7.11-1
+apt-get install -y python-nova=2:13.1.4-0ubuntu4.2 libnl-3-200=3.2.27-1 python
+apt-get install -y python-minimal=2.7.11-1 libpython-stdlib=2.7.11-1 --allow-downgrades
+
+apt-get install -y libpython-dev=2.7.11-1
+apt-get install -y python=2.7.11-1 
+apt-get install -y python-dev=2.7.11-1 
 echo "Reboot for changes to take effect..."
 
